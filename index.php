7606c7ed7b21cd1e037f44f5dea64b635a48b095
Atulpavaiya <115935431+Atulpavaiya@users.noreply.github.com>
2023-08-17 16:43:42 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,223 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>Prestige Institute of Management & Research, Gwalior</title>
+  <link href="style.css" rel="stylesheet" type="text/css" />
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;600;700&display=swap"
+    rel="stylesheet">
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+
+</head>
+
+<body>
+  <section class="header">
+    <nav>
+      <a href="index.html"><img src=""></a>
+      <div class="nav-links" id="navLinks">
+        <i class="fa-solid fa-xmark" onclick="hideMenu()"></i>
+
+        <ul>
+          <li><a href="index.php">HOME</a></li>
+          <li><a href="about.php">ABOUT</a></li>
+          <li><a href="course.php">COURSE</a></li>
+          <li><a href="blog.php">BLOG</a></li>
+          <li><a href="https://www.prestigegwl.org/">SYLLABUS</a></li>
+          <li><a href="https://www.prestigegwl.org/all_upcoming_events.php">UPCOMING EVENTS</a></li>
+          <li><a href="https://www.prestigegwl.org/placement-desk.php">PLACEMENT</a></li>
+          <li><a href="contact.php">CONTACT</a></li>
+          <li>
+            <div>
+              <button onclick="darkMode()">Darkmode</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <i class="fa-solid fa-bars" onclick="showMenu()"></i>
+    </nav>
+    <div class="text-box">
+      <h1>Prestige Institute of Management & Research, Gwalior</h1>
+      <p>Prestige Institute of Management and Research Gwalior (UGC NAAC accredited 'A' Grade and An Autonomous
+        Institute) was set up <br> in the year 1997 by Prestige Education Society, Indore registered under the Societies
+        Registration Act 1860.</p>
+      <br>
+      <br>
+      <a href="login.php" class="hero-btn">Enroll Now</a>
+    </div>
+  </section>
+  <!--   Course -->
+  <section class="course">
+    <h1>Courses We Offer</h1>
+    <div class="row">
+      <div class="course-col">
+        <h3>Management Programs</h3>
+        <p>As a generalist degree, an MBA program gives you fundamental management knowledge, meaning you'll get a
+          holistic view of business across areas like marketing, finance, and accounting, all while developing those
+          vital soft skills and leadership skills.</p>
+      </div>
+      <div class="course-col">
+        <h3>Computer Application Programs</h3>
+        <p>The BCA course is a full time three years (six semesters) Bachelor's Degree in Computer Application. The
+          basic objective of BCA Course is to provide young men and women with the required knowledge and necessary
+          skills to get rewarding careers into the changing world of Information Technology.</p>
+      </div>
+      <div class="course-col">
+        <h3>Commerce Programs</h3>
+        <p>B Com stands for Bachelor of Commerce which is a three year undergraduate program. B Com subjects impart
+          Analytical skills, Financial Literacy, Business Acumen, Business Laws, Taxation knowledge etc, with aim to
+          make the student job ready in the field of Banking, Accounting, Insurance and Finance</p>
+      </div>
+      <!--        <div class="course-col">
+      <h3>Law Programs</h3>
+        <p>BA LLB or Bachelor of Legislative Law is an undergraduate degree, which focuses on integrating knowledge regarding law and other subjects to the candidates. Students are also introduced to law case studies, which will help them practice for their career in the law field.</p>
+      </div> -->
+      <!--       campus -->
+    </div>
+  </section>
+  <section class="campus">
+    <h1>Our Campus</h1>
+
+    <div class="row">
+      <div class="campus-col">
+        <img src="img/3.jpg" alt="Rishabh Mangal">
+        <div class="layer">
+          <h3>Library</h3>
+        </div>
+      </div>
+      <div class="campus-col">
+        <img src="img/2hiha.jpg" alt="Raushan BCA">
+        <div class="layer">
+          <h3>Library</h3>
+        </div>
+      </div>
+      <div class="campus-col">
+        <img src="img/Libraraay.jpg" alt="Surya Pratap Singh">
+        <div class="layer">
+          <h3>Library</h3>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Facilities -->
+  <section class="campus">
+    <h1>Recent Events</h1>
+
+    <div class="row">
+      <div class="campus-col">
+        <img src="img/resize-4.jpg" alt="Rishabh Mangal">
+        <div class="layer">
+          <h3>Spandan Cultural Night - 2017</h3>
+        </div>
+      </div>
+      <div class="campus-col">
+        <img src="img/resize-5.jpg" alt="Raushan BCA">
+        <div class="layer">
+          <h3>SPANDAN 2021</h3>
+        </div>
+      </div>
+      <div class="campus-col">
+        <img src="img/resize-6.jpg" alt="Surya Pratap Singh">
+        <div class="layer">
+          <h3>Student</h3>
+        </div>
+      </div>
+    </div>
+
+
+  </section>
+  <!--   Testimonials? -->
+  <section class="testimonials">
+    <h1>What Our Students Say</h1>
+    <div class="row">
+      <div class="testimonials-col">
+        <img src="img/piiiimg.jpg" alt="Manish">
+        <div>
+          <p>It was a great time we spent in Prestige institute of management, Gwalior . PIMG has very wonderful faculty
+            and they are very cooperative and campus environment is very positive. Training and placement department
+            continuously providing assistance regarding the placement. PIMG has made the best efforts to provide all the
+            facilities for the students like Faculties, events, sports, etc. The days which we spent in PIMG are the
+            golden days of my life and will remember it for rest of my life. PIMG rocks </p>
+          <h3>Surya Pratap Singh</h3>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star-half-stroke"></i>
+        </div>
+      </div>
+      <div class="testimonials-col">
+        <img src="atulhihaha.jpg" alt="Manish">
+        <div>
+          <p> I would say this is the finest college of Gwalior and they prove this as well. As my personal experience,
+            the faculties have bottomless knowledge of subjects. Give proper assignments, projects to students and
+            always ready to help them to finish. In short, I would say the pedagogy, the environment, the placement
+            support, and the diversity of the batch -all work in perfect sync to groom the students for future and make
+            them industry ready</p>
+          <h3>Atul Pavaiya</h3>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-regular fa-star"></i>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!--   Call To Action -->
+
+  <section class="cta">
+    <div>
+      <h1>Enroll For Our Various Online Courses<br>Anywhere From The Online</h1>
+      <a href="pimg.apk" class="hero-btn">DOWNLOAD APK</a>
+    </div>
+  </section>
+
+
+  <!--   Footer -->
+  <section class="surya">
+    <h2>About Us</h2>
+    <p>
+      WE WILL BUILD THIS WEB WITH THE DIRECTION OF<br>SHRUTI DUBEY MAM
+    </p>
+    <div class="icons">
+      <a href="https://www.youtube.com/channel/UC5SrcNr8woEaIHqijlw3lQg"><i class="fa-brands fa-youtube"></i></a>
+      <a href="https://www.instagram.com/prestige_gwalior/?hl=en"><i class="fa-brands fa-instagram"></i></a>
+      <a href="https://www.facebook.com/Prestige.Institute.Gwalior/"><i class="fa-brands fa-facebook"></i></a>
+      <a href="https://www.linkedin.com/school/prestige-institute-of-management-research-gwalior"><i
+          class="fa-brands fa-linkedin"></i></a>
+    </div>
+
+    <p> Made By <i class="fa-regular fa-heart"></i> Atul Pavaiya</p>
+
+  </section>
+
+  <!--   javascript -->
+  <script>
+    var naLinks = document.getElementById("navLinks");
+    function showMenu() {
+      navLinks.style.right = "0";
+    }
+    function hideMenu() {
+      navLinks.style.right = "-200px";
+    }
+  </script>
+  <script src="script.js"></script>
+
+  <script src="https://replit.com/public/js/replit-badge.js" theme="blue" defer></script>
+  <script>
+    function darkMode() {
+      var element = document.body;
+      element.classList.toggle("dark-mode");
+    }
+  </script>
+</body>
+
+</html>
